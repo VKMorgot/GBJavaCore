9ce189c129f07a1fee131408aa73f124a1bb4d3d
VKMorgot <solerosa@mail.ru>
2022-03-07 03:57:52 +0300

    Самостоятельная работа

---
+++ src/main/java/lesson6/selfwork/EchoClient.java
@@ -0,0 +1,147 @@
+package lesson6.selfwork;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class EchoClient extends JFrame {
+    private final String SERVER_ADDR = "localhost";
+    private final int SERVER_PORT = 8189;
+
+    private JTextField msgInputField;
+    private JTextArea chatArea;
+
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    public EchoClient() {
+        try {
+            openConnection();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        prepareGUI();
+    }
+
+    private void openConnection() throws IOException {
+        socket = new Socket(SERVER_ADDR, SERVER_PORT);
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        String strFromServer = in.readUTF();
+                        if (strFromServer.equalsIgnoreCase("/end")) {
+                            break;
+                        }
+                        chatArea.append(strFromServer);
+                        chatArea.append("\n");
+                    }
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    public void closeConnection() {
+        try {
+            in.close();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        try {
+            out.close();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        try {
+            socket.close();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private void sendMessage() {
+        if (!msgInputField.getText().trim().isEmpty()) {
+            try {
+                out.writeUTF(msgInputField.getText());
+                msgInputField.setText("");
+                msgInputField.grabFocus();
+            } catch (IOException exception) {
+                exception.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Ошибка отправки сообщения");
+            }
+        }
+    }
+
+    public void prepareGUI() {
+        // Параметры окна
+        setBounds(600, 300, 500, 500);
+        setTitle("Клиент");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        // Текстовое поле для ввода сообщений
+        chatArea = new JTextArea();
+        chatArea.setEditable(false);
+        chatArea.setLineWrap(true);
+        add(new JScrollPane(chatArea), BorderLayout.CENTER);
+
+        // Нижняя панель с полем для ввода сообщений и кнопкой отправки сообщений
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        JButton btnSendMsg = new JButton("Отправить");
+        bottomPanel.add(btnSendMsg, BorderLayout.EAST);
+        msgInputField = new JTextField();
+        add(bottomPanel, BorderLayout.SOUTH);
+        bottomPanel.add(msgInputField, BorderLayout.CENTER);
+        btnSendMsg.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sendMessage();
+            }
+        });
+        msgInputField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sendMessage();
+            }
+        });
+
+        // Настраиваем действие на закрытие окна
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                try {
+                    out.writeUTF("/end");
+                    closeConnection();
+                } catch (IOException exception) {
+                    exception.printStackTrace();
+                } finally {
+                    System.exit(0);
+                }
+            }
+        });
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new EchoClient();
+            }
+        });
+    }
+}
